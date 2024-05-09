c53f6845a62a88e36ba74159d17c10b1c384247f
Terence <mirac318@outlook.com>
2024-05-09 14:13:07 +0800

    ex1

---
+++ src/edu/hitsz/GameStart.java
@@ -0,0 +1,14 @@
+package edu.hitsz;
+
+import javax.swing.*;
+
+public class GameStart {
+    private JPanel TopPanel;
+    private JButton Moderate;
+    private JCheckBox volumeCheckBox;
+    private JButton Hard;
+    private JButton Easy;
+    private JPanel ChooseDifficulty;
+
+
+}
